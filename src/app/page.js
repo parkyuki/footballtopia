bc69fda03886dcd778267872749ab78b5aa5bda4
Yuki Park <parkyk1223@naver.com>
2023-09-25 18:40:56 +0900

    디자인 수정

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <>
+    <div className="home">
     hi
-    </>
+    </div>
   )
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,14 +5,12 @@ import logo from '@/assets/img/logo.png'
 const Header = () => {
     return (
         <div className='header'>
-            <div className='logo'>
              <Image 
                 src={logo}
                 alt="logo"
                 width={300}
                 height={300}
-                style={{ width: "100%", height: "auto" }}/>
-            </div>
+            />
             <div>
                 <button className='header-button'>login</button>
                 <button className='header-button'>join</button>
