a9efefbda905cd7d58dda6d82989d62c36545b0c
Yuki Park <parkyk1223@naver.com>
2023-10-05 21:01:44 +0900

    //

--- src/app/page.js
+++ src/app/page.js
@@ -10,14 +10,14 @@ export default function Home() {
 
   useEffect(() => {
     async function fetchData() {
-     try {
-         const res = await fetch("http://localhost:9999/posts");
-         const result = await res.json();
-         setData(result);
-      } catch (error) {
-         console.error('Error fetching data: ', error);
-        throw error;
-      }
+      try {
+        const res = await fetch("http://localhost:9999/posts");
+        const result = await res.json();
+        setData(result);
+       } catch (error) {
+          console.error('Error fetching data: ', error);
+         throw error;
+       }
     }
      fetchData();
   }, []);
@@ -26,50 +26,57 @@ export default function Home() {
   return (
     <div className="home">
       {team ? (
-        <div className='table'>
-          <tr>
-            <th>순위</th>
-            <th style={{width:'50%'}}>클럽</th>
-            <th>경기</th>
-            <th>승</th>
-            <th>무</th>
-            <th>패</th>
-            <th>득점</th>
-            <th>실점</th>
-            <th>득실</th>
-            <th>승점</th>
-          </tr>
-        {team.map((team,key)=>(
-        <tr key={key}>
-          <td>{key+1}</td>
-          <td>
-           <div className='teamName'>
-            <Image src={team.team.logo}
-              alt="logo"
-              width={500}
-              height={500}
-              style={{
-                width: '20%',
-                height: 'auto',
-            }}/>
-            <div>
-             {team.team.abbreviation}
-            </div>
-            </div>
-          </td>
-          <td>{team.stats.gamesPlayed}</td>
-          <td>{team.stats.wins}</td>
-          <td>{team.stats.gamesPlayed-team.stats.losses-team.stats.wins}</td>
-          <td>{team.stats.losses}</td>
-          <td>{team.stats.goalsFor}</td>
-          <td>{team.stats.goalDifference}</td>
-          <td>{team.stats.goalsAgainst}</td>
-          <td>{team.stats.points}</td>
-        </tr>
-        ))}</div>
-      ) : (
-        <Loading/>
-      )}
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>순위</th>
+              <th style={{width:'50%'}}>클럽</th>
+              <th>경기</th>
+              <th>승</th>
+              <th>무</th>
+              <th>패</th>
+              <th>득점</th>
+              <th>실점</th>
+              <th>득실</th>
+              <th>승점</th>
+            </tr>
+          </thead>
+          <tbody>
+            {team.map((team,key)=>(
+              <tr key={key}>
+                <td>{key+1}</td>
+                  <td>
+                    <div className='teamName'>
+                      <Image src={team.team.logo}
+                        alt="logo"
+                        width={500}
+                        height={500}
+                        style={{
+                          width: '20%',
+                          height: 'auto',
+                         }}
+                      />
+                       <div>
+                        {team.team.abbreviation}
+                       </div>
+                    </div>
+                  </td>
+                <td>{team.stats.gamesPlayed}</td>
+                <td>{team.stats.wins}</td>
+                <td>{team.stats.gamesPlayed-team.stats.losses-team.stats.wins}</td>
+                <td>{team.stats.losses}</td>
+                <td>{team.stats.goalsFor}</td>
+                <td>{team.stats.goalDifference}</td>
+                <td>{team.stats.goalsAgainst}</td>
+                <td>{team.stats.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        ) : (
+          <Loading/>
+        )
+      }
     </div>
   )
 }
