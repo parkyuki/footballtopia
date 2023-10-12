b085c8958778fde1bd4b027f17344a182de442f9
Yuki Park <parkyk1223@naver.com>
2023-10-12 18:30:27 +0900

    test topics db

--- src/app/board/[id]/page.js
+++ src/app/board/[id]/page.js
@@ -4,13 +4,13 @@ import Image from 'next/image';
 const page = async (props) => {
 
     //클릭한 팀
-    const teamPath=props.params.id;
+    const teamPath = props.params.id;
     //클릭한 팀 찾아서 데이터 불러오기 
     const res = await fetch("http://localhost:9999/posts");
     const team = await res.json();
-    const teamInfo=team.find((team)=>team.team.abbreviation === teamPath);
-    const logo=teamInfo.team.logo;
-    const info=teamInfo.stats;
+    const teamInfo = team.find((team) => team.team.abbreviation === teamPath);
+    const logo = teamInfo.team.logo;
+    const info = teamInfo.stats;
 
     return (
         <div className='board'>
@@ -20,14 +20,15 @@ const page = async (props) => {
                     alt="logo"
                     width={300}
                     height={300}
-                    />
+                />
                 <div className={`name ${teamPath}`}>
                     {teamInfo.team.name}
                 </div>
                 <div className='info'>
-                    {info.rank}위 {info.wins}승 {info.ties}무 {info.losses}패 {info.points}승점 
-                </div> 
+                    {info.rank}위 {info.wins}승 {info.ties}무 {info.losses}패 {info.points}승점
+                </div>
             </div>
+
         </div>
     );
 };
